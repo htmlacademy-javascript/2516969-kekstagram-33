020e55dc9dd96bc306461a1948d9b14b0dcf0d4a
Alina <alinakubikalex@gmail.com>
2024-10-23 22:06:33 +0200

    решает второе задание

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,18 @@ function stringLengthCheck(myString, maxLength) {
 stringLengthCheck('проверяемая строка', 20);
 stringLengthCheck('проверяемая строка', 18);
 stringLengthCheck('проверяемая строка', 10);
+
+
+function checkPalindrom(myString){
+  let newString = '';
+  myString = myString.replaceAll(' ', '').toLowerCase();
+  for (let i = myString.length - 1; i >= 0; i--) {
+    newString += myString[i];
+  }
+  return newString === myString;
+}
+
+checkPalindrom('топот');
+checkPalindrom('ДовОд');
+checkPalindrom('Кекс');
+checkPalindrom('Лёша на полке клопа нашёл ');
