60835661d993d05a9eab750d80bf9b8369078f2a
Alina <alinakubikalex@gmail.com>
2024-10-24 10:23:31 +0200

    решает дополнительное задание

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,23 @@ checkPalindrom('топот');
 checkPalindrom('ДовОд');
 checkPalindrom('Кекс');
 checkPalindrom('Лёша на полке клопа нашёл ');
+
+
+function findNumber(myString) {
+  let stringNumber = '';
+  if(typeof myString === 'number') {
+    myString = myString.toString();
+  }
+  for (let i = 0; i <= myString.length - 1; i++) {
+    if(myString[i] === '0' || parseInt(myString[i], 10)) {
+      stringNumber += parseInt(myString[i], 10);
+    }
+  }
+  return stringNumber;
+}
+
+findNumber('2023 год');
+findNumber('ECMAScript 2022');
+findNumber('1 кефир, 0.5 батона');
+findNumber('агент 007');
+findNumber(125);
